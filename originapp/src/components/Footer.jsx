29f250ef63c9347e572071051324499d5da1d9ff
Neelakandan Nampoothiri N <neelakandannampoothirin@gmail.com>
2026-06-08 02:23:25 +0530

    updated

--- originapp/src/components/Footer.jsx
+++ originapp/src/components/Footer.jsx
@@ -20,8 +20,8 @@ const Footer = ({theme}) => {
     sm:px-10 lg-px-24 xl:px-40'>
         <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
             <motion.div 
-                initial={{opacity:0 ,x:-30}}
-                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0 ,y:30}}
+                whileInView={{opacity:1, y:0}}
                 transition={{duration:0.8,delay:0.2}}
                 viewport={{once:true}}
             
